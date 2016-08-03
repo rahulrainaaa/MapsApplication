8897070e3c8cf04887be1fc9c4a88e000ea731ff
rahulrainahome <rahulrainahome@live.com>
2016-08-03 21:52:38 +0530

    Initial Commit.

--- app/src/main/java/demo/maps/app/mapsapplication/MapsActivity.java
+++ app/src/main/java/demo/maps/app/mapsapplication/MapsActivity.java
@@ -1,5 +1,7 @@
 package demo.maps.app.mapsapplication;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -9,6 +11,9 @@ import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -28,8 +33,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     LocationManager locationManager = null;
-    MarkerOptions myPosition = null;
     Marker marker = null;
+    Marker srchMarker = null;
+
+    boolean frstSrch = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,70 +47,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-    }
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        mMap.getUiSettings().setZoomControlsEnabled(true);
-        mMap.getUiSettings().setCompassEnabled(true);
-        mMap.getUiSettings().setAllGesturesEnabled(true);
-        mMap.setOnMarkerClickListener(this);
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        marker = mMap.addMarker(new MarkerOptions().position(sydney).title("Current"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        getLocationFromAddress("MG Road, Pune, India");
-
-
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-
-        double latitude = location.getLatitude();
-        double longitude = location.getLongitude();
-        LatLng latLng = new LatLng(latitude, longitude);
-
-        marker.setPosition(latLng);
-
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-
-    }
-
-    private boolean isGooglePlayServicesAvailable() {
-        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
-        if (ConnectionResult.SUCCESS == status) {
-            return true;
-        } else {
-            GooglePlayServicesUtil.getErrorDialog(status, this, 0).show();
-            return false;
-        }
     }
 
     @Override
@@ -135,8 +78,60 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
+        mMap.getUiSettings().setAllGesturesEnabled(true);
+        mMap.setOnMarkerClickListener(this);
+        LatLng latlng = new LatLng(18.5204, 73.8567);
+        marker = mMap.addMarker(new MarkerOptions().position(latlng).title("My Position"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latlng));
+        //getLocationFromAddress("Delhi, India");
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+        LatLng latLng = new LatLng(latitude, longitude);
+
+        marker.setPosition(latLng);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        //mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    private boolean isGooglePlayServicesAvailable() {
+        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (ConnectionResult.SUCCESS == status) {
+            return true;
+        } else {
+            GooglePlayServicesUtil.getErrorDialog(status, this, 0).show();
+            return false;
+        }
+    }
+
+    @Override
     public boolean onMarkerClick(Marker marker) {
-        Toast.makeText(getApplicationContext(), "Map-Marker Event", Toast.LENGTH_SHORT).show();
+        inputTextBox();
+        //Toast.makeText(getApplicationContext(), "Map-Marker Event", Toast.LENGTH_SHORT).show();
         return false;
     }
 
@@ -144,23 +139,63 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         Geocoder coder = new Geocoder(this);
         List<Address> address;
-//        GeoPoint p1 = null;
 
         try {
             address = coder.getFromLocationName(strAddress,5);
             if (address==null) {
-  //              return null;
             }
             Address location=address.get(0);
-            location.getLatitude();
-            location.getLongitude();
+            if(frstSrch)
+            {
+                //First time search only.
+                frstSrch = false;
+                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                srchMarker = mMap.addMarker(new MarkerOptions().position(latLng).title("Searched Point"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+            }
+            else
+            {
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                LatLng latLng = new LatLng(latitude, longitude);
 
-  //          p1 = new GeoPoint((int) (location.getLatitude() * 1E6), (int) (location.getLongitude() * 1E6));
+                srchMarker.setPosition(latLng);
 
-            Toast.makeText(getApplicationContext(), "" + (location.getLatitude() * 1E6) + "" + (int) (location.getLongitude() * 1E6), Toast.LENGTH_LONG).show();
-  //          return p1;
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                //mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    public void inputTextBox()
+    {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        final EditText input = new EditText(this);
+
+        alertDialogBuilder.setView(input);
+
+        alertDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                        MapsActivity.this.getLocationFromAddress(input.getText().toString().trim());
+
+                    }
+                })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+        AlertDialog alertD = alertDialogBuilder.create();
+        alertD.show();
+
+    }
+
+
 }
